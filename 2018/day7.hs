774a1e616f766873dea5d45b76fbaaf0f04ead75
Zuilhof <b.zuilhof@plein.nl>
2018-12-10 18:09:49 +0100

    Finally finished part 2 day 7 ughh

--- 2018/day7.hs
+++ 2018/day7.hs
@@ -7,16 +7,19 @@ import Data.Maybe
 type Task = Char
 type Constraint = (Task, Task)
 type Worker = (Task, Int)
---Step F must be finished before step E can begin.
+
 inputData :: [Constraint]
 inputData = [('C','P'),('V','Q'),('T','X'),('B','U'),('Z','O'),('P','I'),('D','G'),('A','Y'),('R','O'),('J','E'),('N','S'),('X','H'),('F','L'),('S','I'),('W','Q'),('H','K'),('K','Q'),('E','L'),('Q','O'),('U','G'),('L','O'),('Y','G'),('G','I'),('M','I'),('I','O'),('A','N'),('H','O'),('T','O'),('H','U'),('A','I'),('B','R'),('V','T'),('H','M'),('C','A'),('B','G'),('L','Y'),('T','J'),('A','R'),('X','L'),('B','L'),('A','F'),('K','O'),('W','M'),('Z','N'),('Z','S'),('R','K'),('Q','L'),('G','O'),('F','Y'),('V','H'),('E','I'),('W','Y'),('U','I'),('F','K'),('M','O'),('Z','H'),('X','S'),('J','O'),('B','I'),('F','H'),('D','U'),('E','M'),('Z','X'),('P','L'),('W','H'),('C','D'),('A','X'),('Q','I'),('R','Y'),('B','A'),('N','L'),('H','G'),('Y','M'),('L','G'),('G','M'),('Z','R'),('S','Q'),('P','J'),('V','J'),('J','I'),('J','X'),('W','O'),('B','F'),('R','M'),('V','S'),('R','W'),('H','E'),('E','U'),('X','Q'),('N','G'),('T','I'),('L','M'),('H','I'),('U','M'),('C','H'),('P','H'),('J','F'),('A','O'),('X','M'),('H','L'),('W','K')]
--- inputDataS = [('C','A'),('C','F'),('A','B'),('A','D'),('B','E'),('D','E'),('F','E')]
--- ['A','B','C','D','E','F'] 
-getAvailableTask :: [Task] -> [Task] -> Task
-getAvailableTask [] done = 'X'
-getAvailableTask todo done = minimum avail
+
+getAvailableTask :: [Task] -> [Task] -> Maybe Task
+getAvailableTask [] done = Nothing
+getAvailableTask todo done = getMaybeTask avail
                         where   constraints = nub (map snd (getConstraints done))
                                 avail = filter (`notElem` constraints) todo
+
+getMaybeTask :: [Task] -> Maybe Task
+getMaybeTask tasks  | not (null tasks) = Just (minimum tasks)
+                    | otherwise = Nothing
 
 getConstraints :: [Task] -> [Constraint]
 getConstraints done = filter (\x -> fst x `notElem` done) inputData
@@ -25,27 +28,49 @@ calcTasks :: [Task] -> [Task] -> [Task]
 calcTasks [x] _ = [x]
 calcTasks todo done = firedTask : calcTasks todo' done'
                         where 
-                            firedTask = getAvailableTask todo done
+                            firedTask = fromJust (getAvailableTask todo done)
                             todo' = filter (/= firedTask) todo
                             done' = firedTask : done  
-getCost :: Task -> Int
-getCost task = fromJust (elemIndex task ['A'..'Z']) + 1
 
+calcWorkers :: [Worker] -> [Task] -> [Task] -> Int -> Int
+calcWorkers workers todo done c | all (== ('-', 0)) workers && null todo = c
+                                | isJust finishedTask = calcWorkers workers' todo done' c
+                                | ('-',0) `elem` workers && isJust availableTask = calcWorkers workers'' todo' done c
+                                | otherwise = calcWorkers (workerNext workers) todo done (c+1)
+                                where 
+                                    finishedTask = getReadyTask workers
+                                    done' = fromJust finishedTask : done 
+                                    workers' = removeTask workers (fromJust finishedTask)
+                                    workers'' = assignTask workers (fromJust availableTask)
+                                    availableTask = getAvailableTask todo done
+                                    todo' = filter (/= fromJust availableTask) todo
 
--- resultP2 = work 0 ('X',0) ('X',0) ['A'..'Z'] []
+workerNext :: [Worker] -> [Worker]
+workerNext [] = []
+workerNext (x:xs)   | ta /= '-' = (ta, ti-1) : workerNext xs
+                    | otherwise = x : workerNext xs
+                    where (ta, ti) = x
 
--- work :: Int -> Worker -> Worker -> [Task] -> [Task] -> [Task]
--- work time (ta1,ti1) (ta2,ti2) todo done | ti1 == 0 =
+removeTask :: [Worker] -> Task -> [Worker]
+removeTask [] _ = []
+removeTask (x:xs) rta   | rta == ta = ('-',0) : xs
+                        | otherwise = x : removeTask xs rta
+                where (ta,ti) = x
 
-calcWorkers :: [Worker] -> [Task] -> [Task] -> ([Worker],[Task],[Task])
-calcWorkers [] _ _ = ([],[],[])
-calcWorkers (x:xs) todo done  | ti == 0 = mergeW (firedTask, getCost firedTask) (calcWorkers xs todo' done')
-                            | otherwise = mergeW (ta, ti)(calcWorkers xs todo done)                                 
-                            where 
-                                (ta,ti) = x
-                                firedTask = getAvailableTask todo done
-                                todo' = filter (/= firedTask) todo
-                                done' = firedTask : done
+getReadyTask :: [Worker] -> Maybe Task
+getReadyTask [] = Nothing
+getReadyTask (x:xs) | ta /= '-' && ti == 0= Just ta
+                    | otherwise = getReadyTask xs
+                    where (ta,ti) = x
 
-mergeW :: Worker -> ([Worker],[Task],[Task]) -> ([Worker],[Task],[Task])
-mergeW w (workers,todo,done) = (w : workers, todo, done)
+assignTask :: [Worker] -> Task -> [Worker]
+assignTask (x:xs) nt    | ta == '-' = (nt, cost) : xs
+                        | otherwise = x : assignTask xs nt
+                        where 
+                            (ta,ti) = x
+                            cost = fromJust (elemIndex nt ['A'..'Z']) + 61
+resultP1 :: [Task]
+resultP1 = calcTasks ['A'..'Z'] []
+
+resultP2 :: Int
+resultP2 = calcWorkers (replicate 5 ('-',0)) ['A'..'Z'] [] 0
