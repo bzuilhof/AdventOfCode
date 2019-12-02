41d4ec24c5d83639134368467421504c4f152116
Zuilhof <bart.zuilhof@navara.nl>
2019-12-02 19:32:38 +0100

    part 1, day2 2019

---
+++ 2019/day2.hs
@@ -0,0 +1,33 @@
+module Day2 where
+
+import Data.List
+import Data.Char
+import Data.Sequence as Seq
+
+inputData :: Seq Integer
+inputData = fromList [1,0,0,3,1,1,2,3,1,3,4,3,1,5,0,3,2,6,1,19,1,5,19,23,1,13,23,27,1,6,27,31,2,31,13,35,1,9,35,39,2,39,13,43,1,43,10,47,1,47,13,51,2,13,51,55,1,55,9,59,1,59,5,63,1,6,63,67,1,13,67,71,2,71,10,75,1,6,75,79,1,79,10,83,1,5,83,87,2,10,87,91,1,6,91,95,1,9,95,99,1,99,9,103,2,103,10,107,1,5,107,111,1,9,111,115,2,13,115,119,1,119,10,123,1,123,10,127,2,127,10,131,1,5,131,135,1,10,135,139,1,139,2,143,1,6,143,0,99,2,14,0,0]
+inputDataM = fromList [1,12,2,3,1,1,2,3,1,3,4,3,1,5,0,3,2,6,1,19,1,5,19,23,1,13,23,27,1,6,27,31,2,31,13,35,1,9,35,39,2,39,13,43,1,43,10,47,1,47,13,51,2,13,51,55,1,55,9,59,1,59,5,63,1,6,63,67,1,13,67,71,2,71,10,75,1,6,75,79,1,79,10,83,1,5,83,87,2,10,87,91,1,6,91,95,1,9,95,99,1,99,9,103,2,103,10,107,1,5,107,111,1,9,111,115,2,13,115,119,1,119,10,123,1,123,10,127,2,127,10,131,1,5,131,135,1,10,135,139,1,139,2,143,1,6,143,0,99,2,14,0,0]
+testData1 = fromList [1,0,0,0,99]
+testData2 = fromList [2,3,0,3,99]
+testData3 = fromList [2,4,4,5,99,0]
+
+
+evolveProgram :: Int -> Seq Integer -> Seq Integer
+evolveProgram cursor program    | mode == 99 = program
+                                | mode == 1 = evolveProgram (cursor + 4) (addition program target val1 val2)
+                                | mode == 2 = evolveProgram (cursor + 4) (multiplication program target val1 val2)
+                            where   mode = fromInteger (index program cursor)
+                                    val1 = fromInteger(indexI program (index program (cursor + 1)))
+                                    val2 = fromInteger(indexI program (index program (cursor + 2)))
+                                    target = fromInteger (index program (cursor + 3))                                   
+
+
+addition, multiplication :: Seq Integer -> Int -> Int -> Int -> Seq Integer
+addition program target val1 val2 = update target (toInteger (val1 + val2))program
+multiplication program target val1 val2 = update target (toInteger (val1 * val2)) program
+
+indexI :: Seq a -> Integer -> a
+indexI sequence i = index sequence (fromInteger i)
+
+updateI :: Integer -> a -> Seq a -> Seq a
+updateI i = update (fromInteger i)
