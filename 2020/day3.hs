5abc4b52f153161a9e09430f01ba8f3fe6f100b4
Bart Zuilhof <bart_zuilhof@hotmail.com>
2020-12-03 10:44:55 +0100

    clean up day 3 2020

--- 2020/day3.hs
+++ 2020/day3.hs
@@ -1,3 +1,4 @@
+{-# LANGUAGE TupleSections #-}
 module Day3 where
 
 inputData, inputDataTest :: [String]
@@ -13,18 +14,23 @@ getXCoords' (x:xs) i | x == '#' = i : getXCoords' xs (i+1)
 getXCoords' [] _ = []
 
 addYCoords :: Int -> [Int] -> [(Int, Int)]
-addYCoords y xs = map (\x -> (x,y)) xs
+addYCoords y = map (,y)
 
 xCoords ::[String] ->  [[Int]]
 xCoords = map getXCoords
 
 getMap :: [String] -> [(Int, Int)]
-getMap input = (concat $ zipWith addYCoords [0..(length (xCoords input))] (xCoords input))
+getMap input = concat $ zipWith addYCoords [0..(length (xCoords input))] (xCoords input)
 
-getPath :: Int -> Int -> [(Int,Int)]
-getPath l h = map (\y -> (y*1`mod`l,y*1))  [0..h]
+getPath :: Int -> Int -> Int -> Int -> [(Int,Int)]
+getPath dx dy l h = map (\y -> (y*dx`mod`l,y*dy))  [0..h]
 
-part1 i = length $ filter (`elem` m) path
-    where path = getPath (length (head i)) (length i) 
-          m = getMap i  
+getTreesOnSlope :: Int -> Int -> [String] -> Int
+getTreesOnSlope dx dy input = length $ filter (`elem` m) path
+    where path = getPath dx dy (length (head input)) (length input) 
+          m = getMap input 
+
+part1 :: Int
+part1 = getTreesOnSlope 3 1 inputData
+part2 = getTreesOnSlope 1 1 inputData * getTreesOnSlope 3 1 inputData * getTreesOnSlope 5 1 inputData * getTreesOnSlope 7 1 inputData * getTreesOnSlope 1 2 inputData
 
