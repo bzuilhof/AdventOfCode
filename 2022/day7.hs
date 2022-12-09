1ea398d99dfb9e3502252e21fbafc15ef957f696
Bart Zuilhof <bart_zuilhof@hotmail.com>
2022-12-09 09:06:03 +0100

    solved day 9, 2022

---
+++ 2022/day7.hs
@@ -0,0 +1,18 @@
+{-# OPTIONS_GHC -Wno-incomplete-patterns #-}
+module Day7 where
+import Data.List
+
+
+testInput = ["cd /","ls","dir a","14848514 b.txt","8504156 c.dat","dir d","cd a","ls","dir e","29116 f","2557 g","62596 h.lst","cd e","ls","584 i","cd ..","cd ..","cd d","ls","4060174 j","8033020 d.log","5626152 d.ext","7214296 k"]
+
+data Loc = FileP Int | Folder [Loc]
+
+parse :: [String] -> [String]-> Loc -> Loc
+parse [] _ t = t
+parse (x:xs) path (t a) | c == "cd" = parse xs (c:path) t
+            where 
+                split = words x
+                c = head split
+
+
+
